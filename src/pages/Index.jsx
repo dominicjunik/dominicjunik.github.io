db205e2fd4c9f79bdc5d84d544b7c2fe2d20ea48
dominicjunik <111475701+dominicjunik@users.noreply.github.com>
2023-09-06 21:53:49 -0600

    finished index page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,17 +5,21 @@ import Pdf from '../assets/resume.pdf'
 
 const Index = () => {
   return (
-    <div className='flex justify-center sm:mt-20'>
+    <div className='flex justify-center sm:mt-20 flex-col-reverse sm:flex-row '>
         <div className='text-green-100 mt-10'>
             <h1 className='text-6xl '>Dominic Junik</h1>
             <h2 className='text-2xl italic'>Junior Software Engineer</h2>
             <div className='flex'>
-                <a href="https://github.com/dominicjunik/" className='hover:text-blue-500 pr-2'><GitHub/></a>
-                <a href="https://www.linkedin.com/in/dominic-junik/" className='hover:text-blue-500 pr-2'><Linkedin/></a>
-                <a href={Pdf} target="_blank" className='hover:text-blue-500 pr-2'><FileText/></a>
+                <a href="https://github.com/dominicjunik/" className='hover:text-blue-500 pr-2' title='GitHub'><GitHub/></a>
+                <a href="https://www.linkedin.com/in/dominic-junik/" className='hover:text-blue-500 pr-2' title='LinkedIn'><Linkedin/></a>
+                <a href={Pdf} target="_blank" className='hover:text-blue-500 pr-2' title='Resume'><FileText/></a>
             </div>
+            <p className='w-full sm:w-96'>Entry level software engineer with experience in full-stack JavaScript web development. Transitioning from a long-term involvement in competitive gaming, possesses a deep passion for coding and its combination of creativity and logical analysis to solve problems.</p>
         </div>
-        <img src={headshot} alt="face" className='h-auto w-1/5'/>
+        <div className='max-w-96 sm:max-w-lg'>
+            <img src={headshot} alt="face" className='object-contain'/>
+        </div>
+       
            
         
        
